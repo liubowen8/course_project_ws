d9982773dc84a659277efc2d2143220125c8d714
greenhand-fighting <1137515540@qq.com>
2021-05-13 10:48:06 +0800

    delete build

--- build/robot_marm/marm_gazebo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/l/course_project_ws/src/robot_marm/marm_gazebo
-# Build directory: /home/l/course_project_ws/build/robot_marm/marm_gazebo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
